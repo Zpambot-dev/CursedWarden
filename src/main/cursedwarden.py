c416035f79e0835afd5f8a2552715095177294ca
Zpambot-dev <74823075+Zpambot-dev@users.noreply.github.com>
2021-03-16 17:23:11 +0300

    Create cursedwarden.py

---
+++ src/main/cursedwarden.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext.commands import Bot
+import os
+
+intents = discord.Intents.all()
+client = Bot(command_prefix="$", intents = intents)
+
+
+@client.event
+async def on_ready():
+    print(f"Logged in as {client.user}")
+
+    await client.change_presence(activity=discord.Game(f"$help| In {len(client.guilds)} servers"), status=discord.Status.do_not_disturb)
+
+for filename in os.listdir(r'C:\Users\Satya\Desktop\Warden\warden\src\main\cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
+        
+client.run()"""Not that open-source lol"""
